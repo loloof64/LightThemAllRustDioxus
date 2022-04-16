097a40183b2b472aa7faee4e56a7072084a9c3c8
Laurent Bernabe <laurent.bernabe@gmail.com>
2022-04-16 22:37:09 +0200

    Defined the light component

---
+++ README.md
@@ -0,0 +1,9 @@
+# Light them all
+
+Try to set all lights on
+
+## Credits
+
+Some vectors have been taken from SvgRepo website:
+* light_off from https://www.svgrepo.com/svg/146885/light-bulb
+* light_on from https://www.svgrepo.com/svg/94654/light-bulb
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,37 @@
+use dioxus::prelude::*;
+
+
 fn main() {
-    println!("Hello, world!");
+    dioxus::desktop::launch(app);
+}
+
+fn app(cx: Scope) -> Element {
+    cx.render(rsx! (
+        style { [include_str!("./style.css")] }
+        div { 
+            Light{ lit : false}
+         }
+    ))
+}
+
+#[derive(PartialEq, Props)]
+struct LightProps {
+    lit: bool
+}
+
+#[allow(non_snake_case)]
+fn Light(cx: Scope<LightProps>) -> Element {
+    let image = if cx.props.lit
+        {
+            "src/assets/vectors/light_on.svg"
+        }
+        else {
+            "src/assets/vectors/light_off.svg"
+        };
+    cx.render(rsx!(
+        img {
+            class: "light",
+            src: format_args!("{}", image),
+        }
+    ))
 }
