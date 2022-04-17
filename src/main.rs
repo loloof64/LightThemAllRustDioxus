899c2be1f85eba068506af16a50870d186093459
Laurent Bernabe <laurent.bernabe@gmail.com>
2022-04-17 20:38:04 +0200

    Setting a grid of lights in the board.

--- src/main.rs
+++ src/main.rs
@@ -9,8 +9,36 @@ fn app(cx: Scope) -> Element {
     cx.render(rsx! (
         style { [include_str!("./style.css")] }
         div { 
-            Light{ lit : false}
+            Board {cols_count: 5}
          }
+    ))
+}
+
+#[derive(PartialEq, Props)]
+struct BoardProps {
+    cols_count: u8,
+}
+
+#[allow(non_snake_case)]
+fn Board(cx: Scope<BoardProps>) -> Element {
+    let lights_lit_size = (cx.props.cols_count * cx.props.cols_count) as usize;
+    let lights_lit = use_ref(&cx, || vec![false; lights_lit_size]);
+    let size_px = (cx.props.cols_count as u16) * 100;
+
+    cx.render(rsx!(
+        div {
+            class: "board",
+            width: "{size_px}px",
+            height: "{size_px}px",
+            grid_template: format_args!("repeat({0}, 1fr) / repeat({0}, 1fr)", cx.props.cols_count),
+
+            lights_lit.read().iter().map(|lit| cx.render(rsx!(
+                div {
+                    class: "cell",
+                    Light{lit: *lit}
+                }
+            )))
+        }
     ))
 }
 
