223963c2c4876e2d6b0883c4c8e676a07c59a27f
Laurent Bernabe <laurent.bernabe@gmail.com>
2022-04-19 01:35:04 +0200

    We can choose lights per side count, in range 2..=6

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use dioxus::prelude::*;
+use std::cell::Cell;
 
 mod components;
 mod game_core;
@@ -11,24 +12,50 @@ fn main() {
 
 fn app(cx: Scope) -> Element {
     let scramble_iterations = 10;
+    // side_lights_count must not trigger interface update on change.
+    let side_lights_count: &Cell<u16> = cx.use_hook(|_| Cell::new(3));
     let game_won = use_state(&cx, || false);
     let lights_lit_state = use_ref(&cx, || {
         let mut vector = vec![false; 9];
         scramble(&mut vector, 10);
         vector
     });
+
+    let select_options = (2..=6).map(move |count| rsx!(
+        option {
+            value: format_args!("{}", count),
+            selected: format_args!("{}", count as u16 == side_lights_count.get()),
+            "{count}"
+        }
+    ));
+
     cx.render(rsx! (
         div {
-            button {
-                onclick: move |_| {
-                    game_won.modify(|_| false);
-                    lights_lit_state.write().fill(false);
-                    scramble(&mut lights_lit_state.write(), scramble_iterations);
+            div {
+                class: "level_line",
+                "Lights per side",
+                select {
+                    onchange: |evt| {
+                        if let Ok(new_side_lights_count) = &evt.value.parse::<u16>() {
+                            side_lights_count.set(*new_side_lights_count);
+                        }
+                    },
+                    select_options
                 },
-                "New game"
+                button {
+                    onclick: move |_| {
+                        let new_size = side_lights_count.get() as usize;
+                        let new_size = new_size * new_size;
+                        lights_lit_state.write().resize(new_size, false);
+                        lights_lit_state.write().fill(false);
+                        scramble(&mut lights_lit_state.write(), scramble_iterations);
+                        game_won.modify(|_| false);
+                    },
+                    "New game"
+                }
             }
             Board {
-                cols_count: 3,
+                cols_count: side_lights_count.get() as u16,
                 lights_lit_state: (*lights_lit_state.read()).clone(),
                 ontoggle: |evt: Toggle| {
                     if *game_won.current() { return; }
