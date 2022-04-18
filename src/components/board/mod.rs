50608273a33bb7255ce2355831f978b64bf5d16f
Laurent Bernabe <laurent.bernabe@gmail.com>
2022-04-18 23:57:16 +0200

    We can start a new game with a button

--- src/components/board/mod.rs
+++ src/components/board/mod.rs
@@ -1,41 +1,40 @@
 mod light;
 use dioxus::prelude::*;
-use rand::Rng;
+
+
+#[derive(Debug, Copy, Clone)]
+pub struct Toggle(pub usize);
 
 #[derive(Props)]
 pub struct BoardProps<'a> {
-    cols_count: u8,
-    onwon: EventHandler<'a>
+    cols_count: u16,
+    lights_lit_state: Vec<bool>,
+    ontoggle: EventHandler<'a, Toggle>
 }
 
 #[allow(non_snake_case)]
 pub fn Board<'a>(cx: Scope<'a, BoardProps<'a>>) -> Element {
-    let lights_lit_size = (cx.props.cols_count * cx.props.cols_count) as usize;
-    let game_finished = use_state(&cx, || false);
-    let lights_lit = use_ref(&cx, || scramble(vec![false; lights_lit_size], 10));
-    let size_px = (cx.props.cols_count as u16) * 100;
+    let count_x = cx.props.cols_count;
+    let count_y = (cx.props.lights_lit_state.len() / cx.props.cols_count as usize) as u16;
+    let width = count_x * 100;
+    let height = count_y * 100;
 
     cx.render(rsx!(
         div {
             class: "board",
-            width: "{size_px}px",
-            height: "{size_px}px",
-            grid_template: format_args!("repeat({0}, 1fr) / repeat({0}, 1fr)", cx.props.cols_count),
+            width: "{width}px",
+            height: "{height}px",
+            grid_template: format_args!("repeat({}, 1fr) / repeat({}, 1fr)", count_x, count_y),
             style { [include_str!("./style.css")] },
 
-            lights_lit.read().iter().enumerate().map(|(index, lit)| {
+            cx.props.lights_lit_state.iter().enumerate().map(|(index, lit)| {
                 cx.render(rsx!(
                     div {
                         class: "cell",
                         light::Light{
                             lit: *lit,
                             onclick: move |_| {
-                                if *game_finished.current() { return; }
-                                toggle_light(&mut lights_lit.write(), index);
-                                if is_game_won(&lights_lit.read()) {
-                                    game_finished.modify(|_| true);
-                                    cx.props.onwon.call(());
-                                }
+                                cx.props.ontoggle.call(Toggle(index))
                             },
                         }
                     }
@@ -43,44 +42,4 @@ pub fn Board<'a>(cx: Scope<'a, BoardProps<'a>>) -> Element {
             })
         }
     ))
-}
-
-fn toggle_light(lights_state: &mut Vec<bool>, index: usize) {
-    let state_len = lights_state.len() as u16;
-    let side_size = (state_len as f64).sqrt() as usize;
-    assert_eq!(
-        (side_size * side_size) as u16,
-        state_len,
-        "lights_state's length should be a square"
-    );
-
-    let col = index % side_size;
-    let row = index / side_size;
-
-    lights_state[index] = !lights_state[index];
-    if row > 0 {
-        lights_state[index - side_size] = !lights_state[index - side_size];
-    }
-    if row < (side_size - 1) {
-        lights_state[index + side_size] = !lights_state[index + side_size];
-    }
-    if col > 0 {
-        lights_state[index - 1] = !lights_state[index - 1];
-    }
-    if col < (side_size - 1) {
-        lights_state[index + 1] = !lights_state[index + 1];
-    }
-}
-
-fn scramble(mut lights_state: Vec<bool>, iterations: u8) -> Vec<bool> {
-    let lights_count = lights_state.len();
-    let mut rng = rand::thread_rng();
-    for _ in 0..iterations {
-        toggle_light(&mut lights_state, rng.gen_range(0..lights_count));
-    }
-    lights_state
-}
-
-fn is_game_won(lights_state: &Vec<bool>) -> bool {
-    lights_state.iter().all(|&elt| elt == true)
 }
--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,81 @@
 use dioxus::prelude::*;
 mod components;
 
+use rand::Rng;
+
 fn main() {
     dioxus::desktop::launch(app);
 }
 
 fn app(cx: Scope) -> Element {
+    let scramble_iterations = 10;
     let game_won = use_state(&cx, || false);
+    let lights_lit_state = use_ref(&cx, || {
+        let mut vector =  vec![false; 9];
+        scramble(&mut vector, 10);
+        vector
+    });
     cx.render(rsx! (
         div { 
-            components::WonLabel {game_won: *game_won.current()},
+            button {
+                onclick: move |_| {
+                    game_won.modify(|_| false);
+                    lights_lit_state.write().fill(false);
+                    scramble(&mut lights_lit_state.write(), scramble_iterations);
+                },
+                "New game"
+            }
             components::Board {
                 cols_count: 3,
-                onwon: |_| game_won.modify(|_| true),
+                lights_lit_state: (*lights_lit_state.read()).clone(),
+                ontoggle: |evt: components::Toggle| {
+                    if *game_won.current() { return; }
+                    toggle_light(&mut lights_lit_state.write(), evt.0);
+                    if is_game_won(&lights_lit_state.read()) {
+                        game_won.modify(|_| true)
+                    }
+                },
             }
+            components::WonLabel {game_won: *game_won.current()},
          }
     ))
+}
+
+fn toggle_light(lights_state: &mut Vec<bool>, index: usize) {
+    let state_len = lights_state.len() as u16;
+    let side_size = (state_len as f64).sqrt() as usize;
+    assert_eq!(
+        (side_size * side_size) as u16,
+        state_len,
+        "lights_state's length should be a square"
+    );
+
+    let col = index % side_size;
+    let row = index / side_size;
+
+    lights_state[index] = !lights_state[index];
+    if row > 0 {
+        lights_state[index - side_size] = !lights_state[index - side_size];
+    }
+    if row < (side_size - 1) {
+        lights_state[index + side_size] = !lights_state[index + side_size];
+    }
+    if col > 0 {
+        lights_state[index - 1] = !lights_state[index - 1];
+    }
+    if col < (side_size - 1) {
+        lights_state[index + 1] = !lights_state[index + 1];
+    }
+}
+
+fn scramble(mut lights_state: &mut Vec<bool>, iterations: u8) {
+    let lights_count = lights_state.len();
+    let mut rng = rand::thread_rng();
+    for _ in 0..iterations {
+        toggle_light(&mut lights_state, rng.gen_range(0..lights_count));
+    }
+}
+
+fn is_game_won(lights_state: &Vec<bool>) -> bool {
+    lights_state.iter().all(|&elt| elt == true)
 }
