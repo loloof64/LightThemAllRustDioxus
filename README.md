1d6f3d6542cc79169fbea3f270c21d28f278117b
Laurent Bernabe <laurent.bernabe@gmail.com>
2022-04-18 14:27:15 +0200

    Implementing the real light's toggling effect.

--- README.md
+++ README.md
@@ -1,6 +1,17 @@
 # Light them all
 
-Try to set all lights on
+Try to set all lights on.
+
+## Game rules.
+
+At the beginning of the game, some lights will be lit. Your goal is to light all of them.
+But, as you toggle a light (by clicking on it), it will toggle the four direct adjacents lights (not those on diagonals) : 
+
+.T.
+TCT
+.T.
+
+Where . is a light that won't have its state changed, C is the clicked light, and T is a light which also will have its state toggled.
 
 ## Credits
 
--- src/components/board/mod.rs
+++ src/components/board/mod.rs
@@ -1,5 +1,5 @@
-use dioxus::prelude::*;
 use super::light::Light;
+use dioxus::prelude::*;
 
 #[derive(PartialEq, Props)]
 pub struct BoardProps {
@@ -21,17 +21,43 @@ pub fn Board<'a>(cx: Scope<'a, BoardProps>) -> Element {
             style { [include_str!("./style.css")] },
 
             lights_lit.read().iter().enumerate().map(|(index, lit)| {
-                let lights_lit_clone = lights_lit.read().clone();
                 cx.render(rsx!(
                     div {
                         class: "cell",
                         Light{
                             lit: *lit,
-                            onclick: move |_| lights_lit.write()[index] = !lights_lit_clone[index],
+                            onclick: move |_| toggle_light(&mut lights_lit.write(), index),
                         }
                     }
                 ))
             })
         }
     ))
+}
+
+fn toggle_light(lights_state: &mut Vec<bool>, index: usize) {
+    let state_len = lights_state.len() as u16;
+    let side_size = (state_len as f64).sqrt() as usize;
+    assert_eq!(
+        (side_size * side_size) as u16,
+        state_len,
+        "lights_state's length should be a square"
+    );
+
+    let col = index % side_size;
+    let row = index / side_size;
+
+    lights_state[index] = !lights_state[index];
+    if row > 0 {
+        lights_state[index - side_size] = !lights_state[index - side_size];
+    }
+    if row < (side_size - 1) {
+        lights_state[index + side_size] = !lights_state[index + side_size];
+    }
+    if col > 0 {
+        lights_state[index - 1] = !lights_state[index - 1];
+    }
+    if col < (side_size + 1) {
+        lights_state[index + 1] = !lights_state[index + 1];
+    }
 }
